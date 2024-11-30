a114dec3ab4fd00ce7bb0b093c1f0c75500259d5
AnkurVermaOfficial <Ankur_ashlynn@outlook.com>
2024-11-30 16:56:39 +0530

    init

---
+++ library/admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; <?php echo date('Y');?> Online Library Management System |<a href="error.html" target="_blank"  > Designed by : LU LEGENDER's</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
